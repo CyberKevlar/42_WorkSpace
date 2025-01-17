0020473a9685ba3cac8db59c40d20eadc834ea84
jmartos- <jcmv.42malaga@gmail.com>
2025-01-17 18:01:57 +0100

    ex02 in progress

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmartos- <jmartos-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/17 16:43:26 by jmartos-          #+#    #+#             */
+/*   Updated: 2025/01/17 16:50:20 by jmartos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+class MutantStack : public std::stack<int>
+{
+private:
+    /* data */
+public:
+    MutantStack : public std::stack<int>(/* args */);
+    ~MutantStack : public std::stack<int>();
+};
+
+MutantStack : public std::stack<int>::MutantStack : public std::stack<int>(/* args */)
+{
+}
+
+MutantStack : public std::stack<int>::~MutantStack : public std::stack<int>()
+{
+}
+
+
+#include "MutantStack.tpp"
+
+#endif
