713b8cb5e2914595c38628d87b09dd952e4e05c2
jmartos- <jcmv.42malaga@gmail.com>
2025-02-10 15:29:24 +0100

    only some things

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: jmartos- <jmartos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 11:12:17 by jmartos-          #+#    #+#             */
-/*   Updated: 2025/02/07 23:03:53 by jmartos-         ###   ########.fr       */
+/*   Updated: 2025/02/10 15:27:09 by jmartos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,12 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &src)
     return (*this);
 }
 
+std::string PmergeMe::itoa(int number) {
+    std::stringstream ascii;
+    ascii << number;
+    return ascii.str();
+}
+
 int PmergeMe::errorControl(int ac, char **av)
 {
     if (ac < 3) {
@@ -57,7 +63,7 @@ int PmergeMe::errorControl(int ac, char **av)
     return 0;
 }
 
-void PmergeMe::fillPairs(std::vector<std::pair<int, int> > &v, std::deque<std::pair<int, int> > &d)
+void PmergeMe::makePairs(std::vector<std::pair<int, int> > &v, std::deque<std::pair<int, int> > &d)
 {
     std::cout << std::endl;
     for (size_t i = 0; i < v.size(); i++) {
@@ -71,39 +77,118 @@ void PmergeMe::fillPairs(std::vector<std::pair<int, int> > &v, std::deque<std::p
     }
 }
 
+void PmergeMe::separate(std::deque<std::pair<int, int> > &d)
+{
+    for (size_t i = 0; i < d.size(); i++) {
+        if (d[i].first < d[i].second) {
+            _v.push_back(d[i].first);
+            _d.push_back(d[i].second);
+        } else {
+            _v.push_back(d[i].second);
+            _d.push_back(d[i].first);
+        }
+    }
+}
+
+void PmergeMe::orderVector(containerV &_v)
+{
+    for (size_t i = 0; i < _v.size(); i++)
+    {
+        for (size_t j = i + 1; j < _v.size(); j++)
+        {
+            if (_v[i] > _v[j]) {
+                // Intercambiar los elementos
+                int temp = _v[i];
+                _v[i] = _v[j];
+                _v[j] = temp;
+            }
+        }
+    }
+    for (size_t i = 0; i < _v.size(); i++) {
+        std::cout << "-" << _v[i];
+    }
+}
+
+void PmergeMe::orderDeque(containerD &_d)
+{
+    for (size_t i = 0; i < _d.size(); i++)
+    {
+        for (size_t j = i + 1; j < _d.size(); j++)
+        {
+            if (_d[i] > _d[j]) {
+                // Intercambiar los elementos
+                int temp = _d[i];
+                _d[i] = _d[j];
+                _d[j] = temp;
+            }
+        }
+    }
+    for (size_t i = 0; i < _d.size(); i++) {
+        std::cout << "+" << _d[i];
+    }
+}
+
+void PmergeMe::printOrder(std::string last) {
+    std::vector<int> combined(_v.begin(), _v.end());
+    combined.insert(combined.end(), _d.begin(), _d.end());
+    std::sort(combined.begin(), combined.end());
+    for (size_t i = 0; i < combined.size(); i++) {
+        std::cout << " " << combined[i];
+    }
+    if (last != "")
+        std::cout << " " << last;
+}
+
 void PmergeMe::fillContainers(int ac, char **av)
 {
     for (int i = 1; i < ac; i++) {
         _v.push_back(std::atoi(av[i]));
         _d.push_back(std::atoi(av[i]));
     }    
-    std::vector<std::pair<int, int> > v;
-    std::deque<std::pair<int, int> > d;
-    int last = 0;
+    std::vector<std::pair<int, int> > v;    // AQUI GUARDAREMOS LA LISTA DE PARES DESORDENADA.
+    std::deque<std::pair<int, int> > d;     // AQUI GUARDAREMOS LA LISTA DE PARES ORDENADA.
+    std::string last = "";                  // AQUI GUARDAREMOS EL ULTIMO ELEMENTO SI EXISTE.
     for (int i = 1; i < ac; i += 2) {
-        if (!_v[i]) {
-            last = _v[i - 1];
-            break ;
+        if (i + 1 < ac) {
+            v.push_back(std::make_pair(_v[i - 1], _v[i]));
+        } else {
+            last = itoa(_v[i - 1]);
+            break;
         }
-        v.push_back(std::make_pair(_v[i - 1], _v[i]));
     }
+    // COMPROBACION QUE SE HAN RELLENADO BIEN EL CONTENEDOR 'V' DE PARES, EL DESORDENADO.
     for (size_t i = 0; i < v.size(); i++) {
         std::cout << "(" << v[i].first << " - " << v[i].second << ")";
     }
+    // ORDENADOMOS EL CONTENEDOR 'V' E INSERTAMOS LOS CAMBIO EN EL 'D'.
+    makePairs(v, d);
+    // COMPROBACION QUE SE HAN RELLENADO BIEN EL CONTENEDOR 'D' DE PARES, ORDENADO DE '-' A '+'.
+    for (size_t i = 0; i < d.size(); i++) {
+        std::cout << "(" << d[i].first << " - " << d[i].second << ")";
+    }
     std::cout << std::endl;
-    fillPairs(v, d);
+    // BORRAMOS LA TOTALIDAD DE LOS ELEMENTOS DE LOS CONTENEDORES DE LAS CLASES PRIVADAS.
     for (int i = 1; i < ac; i++) {
         _v.pop_back();
         _d.pop_back();
     }
-    for (size_t i = 0; i < d.size(); i++) {
-        _v.push_back(d[i].first);
-        _d.push_back(d[i].second);
-        std::cout << "(" << _v[i] << "-" << _d[i] << ")";
+    // SEPARAMOS LOS ELEMENTOS GRANDES DE LOS PEQUEÑOS PARA METERLOS EN LA CLASE PRIVADA
+    separate(d);
+    // COMPROBACION QUE SE HAN RELLENADO BIEN EL CONTENEDOR PRIVADO '_V' CON LOS MENORES.
+    for (size_t i = 0; i < _v.size(); i++) {
+        std::cout << "-(" << _v[i] << ")";
     }
     std::cout << std::endl;
-    std::cout << "Last: " << last << std::endl;
-    int size = _d.size();
-    std::cout << "Size: " << size << std::endl;
-    
+    // COMPROBACION QUE SE HAN RELLENADO BIEN EL CONTENEDOR PRIVADO '_D' CON LOS MAYORES.
+    for (size_t i = 0; i < _d.size(); i++) {
+        std::cout << "+(" << _d[i] << ")";
+    }
+    std::cout << std::endl;
+    // ORDENAMOS LOS CONTENEDORES PRIVADOS '_V' Y '_D' Y LOS MOSTRAMOS, AL IGUAL QUE LAST (SI EXISTE).
+    orderVector(_v);
+    std::cout << std::endl;
+    orderDeque(_d);
+    std::cout << std::endl;
+    printOrder(last);
+    std::cout << std::endl;
 }
--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -6,15 +6,18 @@
 /*   By: jmartos- <jmartos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 16:01:55 by jmartos-          #+#    #+#             */
-/*   Updated: 2025/02/07 22:15:20 by jmartos-         ###   ########.fr       */
+/*   Updated: 2025/02/10 15:14:06 by jmartos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <string>   // std::string
-#include <cstdlib>  // std::atoi
-#include <vector>
-#include <deque>
+#include <string>       // std::string y std::to_string
+#include <cstdlib>      // std::atoi
+#include <vector>       // std::vector<int> 
+#include <deque>        // std::deque<int>
+#include <utility>      // std::pair
+#include <algorithm>    // std::sort
+#include <sstream>      // std::stringstream
 
 class PmergeMe
 {
@@ -31,8 +34,12 @@ class PmergeMe
         ~PmergeMe();
         PmergeMe(const PmergeMe &copy);
         PmergeMe &operator=(const PmergeMe &src);
+        std::string itoa(int number);
         int errorControl(int ac, char **av);
-        void fillPairs(std::vector<std::pair<int, int> > &v, std::deque<std::pair<int, int> > &d);
-        void mercheDeque(std::deque<std::pair<int, int> > &d, int size);
+        void makePairs(std::vector<std::pair<int, int> > &v, std::deque<std::pair<int, int> > &d);
+        void separate(std::deque<std::pair<int, int> > &d);
+        void orderVector(containerV &_v);
+        void orderDeque(containerD &_d);
+        void printOrder(std::string last);
         void fillContainers(int ac, char **av);
 };
