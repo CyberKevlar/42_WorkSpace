707e18ef695849cc3ce2f5d8054a9dc0f6948e02
BigKevlar <kevlaroscuro@gmail.com>
2025-03-23 21:40:22 +0100

    deleting workspace

--- exam5/cpp_module02/BrickWall.cpp
+++
@@ -1,9 +0,0 @@
-#include "BrickWall.hpp"
-
-BrickWall::BrickWall() : ATarget ("Inconspicuous Red-brick Wall") {}
-
-BrickWall::~BrickWall() {}
-
-BrickWall *BrickWall::clone (void) const {
-    return new BrickWall(*this);
-}
