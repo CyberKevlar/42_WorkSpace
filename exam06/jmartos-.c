f4f77f913027aa796deeb0a97b0f6bcf75d40a6d
jmartos- <jcmv.42malaga@gmail.com>
2025-06-14 16:22:23 +0200

    revisado y OK

--- exam06/jmartos-.c
+++ exam06/jmartos-.c
@@ -6,20 +6,20 @@
 #include <sys/select.h>
 #include <netinet/in.h>
 
-typedef struct  s_client
+typedef struct s_client
 {
-    int     id;
-    char    msg[370000];
-}               t_client;
+    int id;
+    char msg[370000];
+} t_client;
 
-t_client    clients[2048];
-fd_set      setCurrent;
-fd_set      setRead;
-fd_set      setWrite;
-int         maxFd = 0;
-int         nextId = 0;
-char        bufferRead[400000];
-char        bufferWrite[400000];
+t_client clients[2048];
+fd_set setCurrent;
+fd_set setRead;
+fd_set setWrite;
+int maxFd = 0;
+int nextId = 0;
+char bufferRead[400000];
+char bufferWrite[400000];
 
 void exitError(char *str)
 {
@@ -78,7 +78,7 @@ int main(int ac, char **av)
         setRead = setWrite = setCurrent;
 
         if (select(maxFd + 1, &setRead, &setWrite, 0, 0) == -1)
-            continue ;
+            continue;
 
         for (int fd = 0; fd <= maxFd; fd++)
         {
@@ -95,33 +95,33 @@ int main(int ac, char **av)
                     FD_SET(clientFd, &setCurrent);
                     sprintf(bufferWrite, "server: client %d just arrived\n", clients[clientFd].id);
                     sendAll(clientFd);
-                    break ;
-                }
-            }
-            else
-            {
-                int readBytes = recv(fd, bufferRead, sizeof(bufferRead), 0);
-                if (readBytes <= 0)
-                {
-                    sprintf(bufferWrite, "server: client %d just left\n", clients[fd].id);
-                    sendAll(fd);
-                    FD_CLR(fd, &setCurrent);
-                    bzero(clients[fd].msg, strlen(clients[fd].msg));
-                    close(fd);
-                    break ;
+                    break;
                 }
                 else
                 {
-                    for (int i = 0, j = strlen(clients[fd].msg); i < readBytes; i++, j++)
+                    int readBytes = recv(fd, bufferRead, sizeof(bufferRead), 0);
+                    if (readBytes <= 0)
                     {
-                        clients[fd].msg[j] = bufferRead[i];
-                        if (clients[fd].msg[j] == '\n')
+                        sprintf(bufferWrite, "server: client %d just left\n", clients[fd].id);
+                        sendAll(fd);
+                        FD_CLR(fd, &setCurrent);
+                        bzero(clients[fd].msg, strlen(clients[fd].msg));
+                        close(fd);
+                        break;
+                    }
+                    else
+                    {
+                        for (int i = 0, j = strlen(clients[fd].msg); i < readBytes; i++, j++)
                         {
-                            clients[fd].msg[j] = '\0';
-                            sprintf(bufferWrite, "client %d: %s\n", clients[fd].id, clients[fd].msg);
-                            sendAll(fd);
-                            bzero(clients[fd].msg, strlen(clients[fd].msg));
-                            j = -1;
+                            clients[fd].msg[j] = bufferRead[i];
+                            if (clients[fd].msg[j] == '\n')
+                            {
+                                clients[fd].msg[j] = '\0';
+                                sprintf(bufferWrite, "client %d: %s\n", clients[fd].id, clients[fd].msg);
+                                sendAll(fd);
+                                bzero(clients[fd].msg, strlen(clients[fd].msg));
+                                j = -1;
+                            }
                         }
                     }
                 }
